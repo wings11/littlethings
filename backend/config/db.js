84f0f22c63152d04bff9c5a4092b20934520d638
wings11 <wykyaw2001@gmail.com>
2025-03-11 01:15:24 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || sql309.infinityfree.com,
+  user: process.env.DB_USER || if0_38487499,
+  password: process.env.DB_PASSWORD || pU4dfArkwB9g2,
+  database: process.env.DB_NAME || if0_38487499_littlethings,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
